c0d5d90e2160d06f2a60bec55b17cfc7728fce4c
WCIII <warrenlathe@me.com>
2024-02-03 12:43:10 -1000

    added route code, doesn't work yet

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -3,16 +3,38 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
+
 // get all products
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [Category, Tag]
+  })
+    .then((products) => {
+      res.json(products)
+    })
+    .catch((err) => {
+      res.json(err)
+    })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+    Product.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [Category, Tag]
+    })
+      .then((product) => {
+        res.json(product)
+      })
+      .catch((err) => {
+        res.json(err)
+      })
 });
 
 // create new product
@@ -94,6 +116,17 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then((product) => {
+      res.json(product)
+    })
+    .catch((err) => {
+      res.json(err)
+    })
 });
 
 module.exports = router;
